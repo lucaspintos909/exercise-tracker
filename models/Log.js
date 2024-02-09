7e6b5f08c8ce5851435c2f24379d8bde41313d6f
lucaspintos909 <lucaspintos909@gmail.com>
2024-02-09 14:54:27 +0000

    feat: Agregando schema y modelo Log

---
+++ models/Log.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// Schema
+let LogSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  count: Number,
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: Date,
+    },
+  ],
+});
+
+// Model
+let LogModel = mongoose.model("log", LogSchema);
+
+export { LogModel };
