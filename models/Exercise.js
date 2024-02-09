57930df9274f62bc887c67f60efdf4e8092dace5
lucaspintos909 <lucaspintos909@gmail.com>
2024-02-09 14:54:16 +0000

    feat: Agregando schema y modelo Exercise

---
+++ models/Exercise.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// Schema
+let ExerciseSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: Date,
+});
+
+// Model
+let ExerciseModel = mongoose.model("exercise", ExerciseSchema);
+
+export { ExerciseModel };
