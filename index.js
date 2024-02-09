d63b576698e88f1c9cccdd41c740529e5f3aa61d
lucaspintos909 <lucaspintos909@gmail.com>
2024-02-09 22:09:54 +0000

    feat: Agregando rutas personalizadas

--- index.js
+++ index.js
@@ -24,13 +24,16 @@ app.get("/", function (req, res) {
 });
 
 app.use(cors());
+
 app.use(express.static("public"));
-
-
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/users", require("./routes/exercises"));
+app.use("/api/users", require("./routes/logs"));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
